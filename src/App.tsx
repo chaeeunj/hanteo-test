08781d7eee70994f66f957a95dc176bd9da19057
chaeeunj <official2930@naver.com>
2025-02-09 01:38:40 +0900

    react-query 설치 & QueryClientProvider 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router';
+import Home from './pages/Home';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 function App() {
-  return <></>;
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
