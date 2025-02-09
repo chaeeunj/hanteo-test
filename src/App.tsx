0dda1b302e1899d110a74a73df19c2501b32e91b
chaeeunj <official2930@naver.com>
2025-02-10 02:52:26 +0900

    Feat: 카테고리별 페이지 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Layout from './components/layout/Layout';
-import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Layout from "./components/layout/Layout";
+import Home from "./pages/Home";
+import Whook from "./pages/Whook";
+import Event from "./pages/Event";
+import News from "./pages/News";
+import Store from "./pages/Store";
+import ChargingStation from "./pages/ChargingStation";
 
 function App() {
   const queryClient = new QueryClient();
@@ -12,6 +17,11 @@ function App() {
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/whook" element={<Whook />} />
+            <Route path="/event" element={<Event />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/store" element={<Store />} />
+            <Route path="/charging-station" element={<ChargingStation />} />
           </Routes>
         </Layout>
       </BrowserRouter>
