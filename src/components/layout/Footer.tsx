f0dcf969a29145e069dae733321ba8ce17f5eae9
chaeeunj <official2930@naver.com>
2025-02-09 21:58:50 +0900

    Feat: Footer 컴포넌트 구현

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer className="bg-gray-200 w-full h-full px-5 py-3">
+      <h1 className="text-slate-800 font-semibold text-lg">Chaeeun Jung</h1>
+      <ul className="mt-3 flex flex-col text-slate-800 text-sm">
+        <li>
+          <a href="mailto:official2930@naver.com">official2930@naver.com</a>
+        </li>
+        <li>
+          <a href="https://github.com/chaeeunj/hanteo-test">깃허브 바로가기</a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
