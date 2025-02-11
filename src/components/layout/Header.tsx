b47d0fa35c442c1a6f6184aaf1639366ad8a9ab6
chaeeunj <official2930@naver.com>
2025-02-11 21:11:39 +0900

    Refactor: 외부에서 객체 받아오는 방식으로 변경

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,24 +1,11 @@
 import { Link } from "react-router-dom";
-
-interface MenuList {
-  link: string;
-  menu: string;
-}
-
-const menuList: MenuList[] = [
-  { link: "/", menu: "차트" },
-  { link: "/whook", menu: "Whook" },
-  { link: "/event", menu: "이벤트" },
-  { link: "/news", menu: "뉴스" },
-  { link: "/store", menu: "스토어" },
-  { link: "/charging-station", menu: "충전소" },
-];
+import { menuList } from "../../constants/header";
 
 function Header() {
   return (
-    <header className="h-full w-full bg-red-300 px-5 py-3">
+    <header className="h-fit w-full bg-red-300 px-5 py-3">
       <ul className="flex justify-between gap-4">
-        {menuList.map((item: MenuList, idx: number) => (
+        {menuList.map((item, idx: number) => (
           <li key={idx} className="font-bold hover:text-white">
             <Link to={item.link}>{item.menu}</Link>
           </li>
