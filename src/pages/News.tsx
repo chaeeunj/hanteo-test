2c468a893bd209f25aee5e9aa6e2186f5e404d8f
chaeeunj <official2930@naver.com>
2025-02-11 21:26:28 +0900

    Design: 불필요한 컴포넌트 제거

--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -3,7 +3,6 @@ import Content from "../components/common/Content";
 function News() {
   return (
     <div className="flex h-full w-full flex-col">
-      <div className="mt-4 w-full bg-slate-500 p-10">배너영역</div>
       <Content />
     </div>
   );
