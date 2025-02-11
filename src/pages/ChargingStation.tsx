5be92b571782c65c4eeb5c4e8f5fabfb4b3ac43c
chaeeunj <official2930@naver.com>
2025-02-11 21:22:13 +0900

    Feat: CharginStation 페이지 구현

---
+++ src/pages/ChargingStation.tsx
@@ -0,0 +1,11 @@
+import Content from "../components/common/Content";
+
+function ChargingStation() {
+  return (
+    <div className="flex h-full w-full flex-col">
+      <Content />
+    </div>
+  );
+}
+
+export default ChargingStation;
