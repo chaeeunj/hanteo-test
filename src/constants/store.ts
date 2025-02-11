7113fbead73fdd520ab0132907014361de85b22e
chaeeunj <official2930@naver.com>
2025-02-11 20:27:25 +0900

    Feat: 스토어 페이지에 사용될 객체 & 타입 작성

---
+++ src/constants/store.ts
@@ -0,0 +1,17 @@
+import { StoreUser, StoreItems } from "../types/store";
+
+export const storeUser: StoreUser = {
+  name: "chae92",
+  coin: 140000,
+};
+
+export const storeItems: StoreItems[] = [
+  { itemName: "Whosfan 골드 투표권 X 25", coin: 615 },
+  { itemName: "Whosfan 골드 투표권 X 10", coin: 245 },
+  { itemName: "Whosfan 골드 투표권 X 5", coin: 130 },
+  { itemName: "Whosfan 골드 투표권 X 1", coin: 25 },
+  { itemName: "Whosfan 실버 투표권 X 25", coin: 375 },
+  { itemName: "Whosfan 골드 투표권 X 10", coin: 155 },
+  { itemName: "Whosfan 골드 투표권 X 5", coin: 80 },
+  { itemName: "Whosfan 골드 투표권 X 1", coin: 15 },
+];
---
+++ src/types/store.ts
@@ -0,0 +1,9 @@
+export interface StoreUser {
+  name: string;
+  coin: number;
+}
+
+export interface Items {
+  itemName: string;
+  coin: number;
+}
